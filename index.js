9871c52964618113dce4643878f37e9837727e5a
Sara Doolin <saradoolin@aol.com>
2019-11-07 22:35:43 -0500

    finished project

--- index.js
+++ index.js
@@ -41,8 +41,23 @@ class Airplane {
 */
 
 class Person {
-
-}
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if(this.stomach.length < 10) {
+      this.stomach.push (someFood);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
+}  
 
 /*
   TASK 2
@@ -59,7 +74,27 @@ class Person {
 */
 
 class Car {
-
+  constructor (model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    const gasUsed = distance/this.milesPerGallon;
+    if (this.tank >= gasUsed) {
+      this.odometer += distance;
+      this.tank -= gasUsed;
+    } else {
+      const milesLeft = this.tank * this.milesPerGallon;
+      this.tank = 0;
+      this.odometer += milesLeft;
+      return `I ran out of fuel at ${this.odometer} miles!`
+      }
+    } 
 }
 
 /*
@@ -75,7 +110,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor (argsObject) {
+    this.name = argsObject.name;
+    this.age = argsObject.age;
+    this.location = argsObject.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}.`
+  }
 }
 
 /*
@@ -92,8 +134,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor (argsObject) {
+    super(argsObject);
+    this.specialty = argsObject.specialty;
+    this.favLanguage = argsObject.favLanguage; 
+    this.catchPhrase = argsObject.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
@@ -111,8 +164,24 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor (argsObject) {
+    super(argsObject);
+    this.previousBackground = argsObject.previousBackground;
+    this.className = argsObject.className; 
+    this.favSubjects = argsObject.favSubjects;
+    this.grade = 85; //strech 
+  }
+  listSubjects() {
+    const subjString = this.favSubjects.join(', ');
+    return `Loving ${subjString}!`;
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
 
 /*
@@ -128,9 +197,20 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
-}
+class ProjectManager extends Instructor {
+  constructor (argsObject) {
+    super(argsObject);
+    this.gradClassName = argsObject.gradClassName;
+    this.favInstructor = argsObject.favInstructor;
+  }
+  standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
+  }
+ 
 
 /*
   STRETCH PROBLEM (no tests!)
